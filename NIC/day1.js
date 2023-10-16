3d09663a7939867983d818c3bf9a31462e026034
DEEPAK11VERMA <rajverma5105@gmail.com>
2023-10-16 16:40:01 +0530

    first commit

---
+++ NIC/day1.js
@@ -0,0 +1,34 @@
+// // // // // // variable
+// // // // // var name = 'Deepak verma ';
+// // // // // // var keyword  deepak verma -> value.
+// // // // // // variable can assign interge char string boolean.
+
+// // // // // //  varrable can start with _ $ and any charcter but not start with number .
+// // // // // // 6 type of data type in javaScript 
+// // // // // // boolean 
+// // // // // // bigInt 
+// // // // // // number 
+// // // // // // String
+// // // // // // undefined
+// // // // // // symbol
+// // // // // console.log(name);
+// // // // let c=30;
+// // // // if(c===30){
+// // // //     let c=31;
+
+// // // // }
+// // // // console.log(c);
+
+// // // var num = 1;
+// // // do{
+// // //     debugger;
+// // //     console.log(num);
+// // //     num++;
+
+// // // }while(num<=20);
+// // var arr=['arun','arun','arun','arun','arun'];
+// // console.log(arr.length-1);
+
+// // console.log(Date.now());
+// let news = new Date();
+// console.log(news);
